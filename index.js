285061547fa0abfc4a14187ce0fa66a2a409e8c1
themirrazz <89490659+themirrazz@users.noreply.github.com>
2025-03-01 10:38:43 -0800

    level 1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //!wrt
+w96.FSUtil.normalizeDriveLetter = function (k) {
+    return k[0].toLowerCase() + k.slice(1);
+};
+
+w96.FSUtil.fixPath = function (e) {
+    let t = e.replace(/(^\/)|(\/$)/gi,"");
+    t = t.replace(/(\/){2,}/gi,"/");
+    return t;
+};
+
+w96.FS.toURL = async function (url) {
+    var blob = await w96.FS.toBlob(url);
+    return URL.createObjectURL(blob);
+};
